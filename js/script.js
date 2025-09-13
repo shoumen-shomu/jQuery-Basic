5313f549946257a0ed515fe7df9c17ef3a830772
Shoumen Shomu <shoumenshomu@gmail.com>
2025-09-13 21:09:14 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,94 @@
+$("#show").click(function(){
+    $(".box").show(1000)
+})
+$("#hide").click(function(){
+    $(".box").hide(1000)
+})
+$("#showhide").click(function(){
+    $(".box").toggle(1000)
+})
+$("#fadeIn").click(function(){
+    $(".box").fadeIn(1000)
+})
+$("#fadeOut").click(function(){
+    $(".box").fadeOut(1000)
+})
+$("#fadeInOut").click(function(){
+    $(".box").fadeToggle(1000)
+})
+$("#slideUp").click(function(){
+    $(".box").slideUp(1000)
+})
+$("#slideDown").click(function(){
+    $(".box").slideDown(1000)
+})
+$("#slideUpDown").click(function(){
+    $(".box").slideToggle(1000)
+})
+$("#new").click(function(){
+    $(".box").addClass("boxOne")
+})
+$("#old").click(function(){
+    $(".box").removeClass("boxOne")
+})
+$("#newOld").click(function(){
+    $(".box").toggleClass("boxOne")
+})
+
+
+// countdown part start
+$(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  //I'm adding this section so I don't have to keep updating this pen every year :-)
+  //remove this if you don't need it
+  let today = new Date(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
+      nextYear = yyyy + 1,
+      dayMonth = "11/27/",
+      birthday = dayMonth + yyyy;
+  
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > birthday) {
+    birthday = dayMonth + nextYear;
+  }
+  //end
+  
+  const countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
+
+        const now = new Date().getTime(),
+              distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          document.getElementById("headline").innerText = "It's my birthday!";
+          document.getElementById("countdown").style.display = "none";
+          document.getElementById("content").style.display = "block";
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
+// Countdown Part End
+
+// counter Part Start
+
+        jQuery(document).ready(function($) {
+            $(".counter").counterUp({
+                delay: 10,
+                time: 1000
+            });
+        });
+
+// counter Part End
